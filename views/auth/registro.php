427d0a8cd8d143b8151200b036a6a19b38a91c9d
jcedronvelasquez <jcedronvelasquez@gmail.com>
2025-05-22 13:14:23 -0500

    modal de exitoso  colocado

--- views/auth/registro.php
+++ views/auth/registro.php
@@ -48,10 +48,42 @@
             <label for="foto">Agregar foto:</label>
             <input type="file" id="foto" name="foto" accept="image/*" required>
 
-            <button type="submit" name="registrar">Registrarse</button>
+            <button type="submit" name="registrar" class="btn btn-success mt-3" onclick="mostrarModal('registroExitosoModal')">
+                Registrarse</button>
         </form>
 
         <p>¿Ya tienes cuenta? <a href="login.php">Inicia sesión</a></p>
     </div>
+
+
+    <!-- Modal de Registro Exitoso -->
+<div class="modal fade" id="registroExitosoModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" style="max-width: 500px;">
+    <div class="modal-content text-center p-4">
+      <div class="modal-body">
+        <i class="bi bi-check-circle-fill" style="font-size: 3rem; color: #28a745;"></i>
+        <h1 class="mt-3">¡Registro Exitoso!</h1>
+        <p class="mt-2">Bienvenidos a <strong>Dog Match</strong></p>
+        <p class="fst-italic">Conectando patas, creando lazos.</p>
+      </div>
+      <div class="modal-footer justify-content-center border-0">
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">¡Vamos allá!</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- script de boostrap para el modal -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+
+
+</script>
+
+<!-- script de registro_modal -->
+<script src="public/js/registro.js">
+
+</script>
+
+
 </body>
 </html>
